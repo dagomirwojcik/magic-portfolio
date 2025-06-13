a073538e40f13c6b4a3c734030e340481f52dae6
Dagomir "Dag" Wojcik <123994953+dagomirwojcik@users.noreply.github.com>
2025-06-13 21:41:31 +0200

    A few changes made

--- src/resources/content.js
+++ src/resources/content.js
@@ -1,16 +1,16 @@
 import { Logo } from "@once-ui-system/core";
 
 const person = {
-  firstName: "Selene",
-  lastName: "Yu",
+  firstName: "Dagomir",
+  lastName: "Wojcik",
   get name() {
     return `${this.firstName} ${this.lastName}`;
   },
-  role: "Design Engineer",
+  role: "University Student",
   avatar: "/images/avatar.jpg",
-  email: "example@gmail.com",
-  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+  email: "dagomir@dagomirwojcik,com",
+  location: "Europe/London", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: ["English", "Polish"], // optional: Leave the array empty if you don't want to display languages
 };
 
 const newsletter = {
