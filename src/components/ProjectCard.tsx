d83effe57f43f44d431f7e0ea8743fcf35a53834
Dagomir "Dag" Wojcik <123994953+dagomirwojcik@users.noreply.github.com>
2025-06-14 21:26:08 +0200

    fixed errors in ProjectCard component

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -32,7 +32,7 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({
 }) => {
   return (
     <Column fillWidth gap="m">
-      <Carousel sizes="(max-width: 960px) 100vw, 960px">
+      <Carousel sizes="(max-width: 960px) 100vw, 960px" items={[]}>
         {images.map((image, idx) => (
           <img key={idx} src={image} alt={title} style={{ width: "100%", height: "auto" }} />
         ))}
